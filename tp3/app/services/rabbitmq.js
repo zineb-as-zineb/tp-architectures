b7155c5f194259124a71f8c4b4398b9eb4fc99c1
Zineb <asimzineb@gmail.com>
2026-04-04 17:29:42 +0100

    fix: tp3 converti en dossier normal

---
+++ tp3/app/services/rabbitmq.js
@@ -0,0 +1,19 @@
+const amqp = require('amqplib'); 
+  
+let channel; 
+const QUEUE = 'commandes'; 
+  
+async function connectRabbitMQ() { 
+  const conn = await amqp.connect('amqp://admin:admin@rabbitmq'); 
+  channel = await conn.createChannel(); 
+  await channel.assertQueue(QUEUE, { durable: true }); 
+  console.log('✅ RabbitMQ connecté'); 
+} 
+  
+function publierCommande(commande) { 
+  const msg = JSON.stringify(commande); 
+  channel.sendToQueue(QUEUE, Buffer.from(msg), { persistent: true }); 
+  console.log('📤 Message publié dans RabbitMQ'); 
+} 
+  
+module.exports = { connectRabbitMQ, publierCommande }; 
