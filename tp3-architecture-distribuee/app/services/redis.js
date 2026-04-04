2955410f8bb08329c9a9db876948993a7da7c496
Zineb <asimzineb@gmail.com>
2026-04-04 17:24:07 +0100

    sync: renommage dossiers + ajout raports tps

--- tp3-architecture-distribuee/app/services/redis.js
+++
@@ -1,7 +0,0 @@
-const Redis = require('ioredis');
-
-const redis = new Redis({ host: 'redis', port: 6379 });
-
-redis.on('connect', () => console.log('✅ Redis connecté'));
-
-module.exports = redis;
